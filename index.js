5230d140a1d45b0290ca89d833a4ad3511d9b9b8
Ryan Tumel <rtumel123@gmail.com>
2020-12-02 11:38:48 -0500

    add joi validation handling

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// import routes
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 // connect to DB
 mongoose.connect(
   process.env.DB_CONNECT,
   { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log('Connected to DB'));
+  () => console.log('Connected to DB')
+);
 
-// import routes
-const authRoute = require('./routes/auth');
+// middleware
+app.use(express.json());
 
-// middlewares
+// route middlewares
 app.use('/api/user', authRoute);
 
 app.listen(3000, () => console.log('Server running'))
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,32 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
-router.post('/register', (req, res) => {
-  res.send('Register');
+// VALIDATION
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required()
+});
+
+router.post('/register', async (req, res) => {
+
+  // validate data before sending
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 module.exports = router;
