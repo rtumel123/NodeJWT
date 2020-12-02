6f91276dafe0f1a8822795221ee4c4d9dbb64309
Ryan Tumel <rtumel123@gmail.com>
2020-12-02 10:59:15 -0500

    connected to db

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// connect to DB
+mongoose.connect('mongodb+srv://test:123@nodejwt.i7q87.mongodb.net/NodeJWT?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log('Connected to DB');
+});
 
 // import routes
 const authRoute = require('./routes/auth');
