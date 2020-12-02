87a43817ec56cd11afc45010718fc0e6d92262cf
Ryan Tumel <rtumel123@gmail.com>
2020-12-02 11:07:37 -0500

    add user model

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // connect to DB
-mongoose.connect('mongodb+srv://test:123@nodejwt.i7q87.mongodb.net/NodeJWT?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-  console.log('Connected to DB');
-});
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('Connected to DB'));
 
 // import routes
 const authRoute = require('./routes/auth');
