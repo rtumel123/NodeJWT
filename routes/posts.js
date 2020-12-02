1cb4e2a88dc72146c71b8763e26b1e25fd6ca054
Ryan Tumel <rtumel123@gmail.com>
2020-12-02 12:31:43 -0500

    logic to find specific user- commented out

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const User = require('../models/User');
 const verify = require('./verifyToken');
 
 router.get('/', verify, (req, res) => {
-  res.json({ posts: { title: 'post', description: 'need login to access' } })
+  res.send(req.user);
+  // User.findOne({ _id: req.user });
 });
 
 module.exports = router;
