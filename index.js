32bd3ce1706d350f8346e31eaa07b5e13a33851d
Ryan Tumel <rtumel123@gmail.com>
2020-12-02 10:40:29 -0500

    initialize auth router

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+// import routes
+const authRoute = require('./routes/auth');
+
+// middlewares
+app.use('/api/user', authRoute);
+
 app.listen(3000, () => console.log('Server running'))
