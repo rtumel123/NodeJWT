adb81416bb24517b07a65b57a97ffda3ebcb8f6f
Ryan Tumel <rtumel123@gmail.com>
2020-12-02 12:00:10 -0500

    add bcrypt for password hashing

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const { registerValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
+const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
   // validate data before sending
@@ -12,20 +13,28 @@ router.post('/register', async (req, res) => {
   if (emailExist) return res.status(400).send('Email already exists');
 
   // hash password
-  
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
   // create new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   });
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: user._id });
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+// LOGIN
+router.post('/login', (req, res) => {
+  // validate data before sending
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 });
 
 module.exports = router;
